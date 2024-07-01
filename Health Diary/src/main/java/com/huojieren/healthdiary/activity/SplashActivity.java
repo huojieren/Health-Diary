998b0e8c1a5ec0dc8d348281cb9f7442c90d3aa7
huojieren <1956934061@qq.com>
2024-07-01 13:35:51 +0800

    feat: 新增应用启动页
    新增应用启动页展示应用名称、logo和版本号，提供启动按钮开始使用应用

---
+++ Health Diary/src/main/java/com/huojieren/healthdiary/activity/SplashActivity.java
@@ -0,0 +1,30 @@
+package com.huojieren.healthdiary.activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.huojieren.healthdiary.MainActivity;
+import com.huojieren.healthdiary.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        Button startButton = findViewById(R.id.start_button);
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent mainIntent = new Intent(SplashActivity.this, MainActivity.class);
+                SplashActivity.this.startActivity(mainIntent);
+                SplashActivity.this.finish();
+            }
+        });
+    }
+}
