ba8b0ece40f9d018037c95edf1ef57e2a9408f24
huojieren <1956934061@qq.com>
2024-07-01 14:39:56 +0800

    perf: 优化代码，发布version1.0

--- Health Diary/src/main/java/com/huojieren/healthdiary/activity/SplashActivity.java
+++ Health Diary/src/main/java/com/huojieren/healthdiary/activity/SplashActivity.java
@@ -2,7 +2,6 @@ package com.huojieren.healthdiary.activity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -18,13 +17,10 @@ public class SplashActivity extends AppCompatActivity {
         setContentView(R.layout.activity_splash);
 
         Button startButton = findViewById(R.id.start_button);
-        startButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent mainIntent = new Intent(SplashActivity.this, MainActivity.class);
-                SplashActivity.this.startActivity(mainIntent);
-                SplashActivity.this.finish();
-            }
+        startButton.setOnClickListener(v -> {
+            Intent mainIntent = new Intent(SplashActivity.this, MainActivity.class);
+            SplashActivity.this.startActivity(mainIntent);
+            SplashActivity.this.finish();
         });
     }
 }
